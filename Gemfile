eb14247c0d126814a82dc15c274676723b18cc91
phoet <phoetmail@googlemail.com>
2011-07-31 16:02:46 +0200

    add couchpreparations

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ source "http://rubygems.org"
 gem "sinatra"
 gem "haml"
 gem "foreman"
+gem "rest-client"
+gem "json"
 
 group :development do
   gem "heroku"
--- lib/application.rb
+++ lib/application.rb
@@ -1,5 +1,33 @@
+# encoding: UTF-8
+# DB = "#{ENV['CLOUDANT_URL']}/cites"
+# 
+# get "/doc/:doc" do
+#   doc = RestClient.get("#{DB}/#{params[:doc]}")
+#   @result = JSON.parse(doc)
+#   haml :doc_id
+# end
 
 get '/' do
+  @cites = [
+    'Tu dir nicht weh!',
+    'Verletz dich nicht!',
+    'Hättste das mal bei mir gemacht!',
+    'Geh schon mal raus, lauf dich warm und fang schon mal an zu bluten.',
+    'Ich mein schon was du weißt.',
+    'I told you, I schmit you down the Kellertreppe and you never come back to the Tageslicht again!',
+    'Mach mal \'nen Satz draus.',
+    'Was ist die Hälfte von Wasser.',
+    'Schrei mich nicht an!',
+    'I break together, I can\'t no more!',
+    'Gänsefleisch mein Koffer tragen...',
+    'Du do ne do!',
+    'Ja ja ja ja, nee.',
+    'Ein Glück, dass Bewegungslegasthenie nicht vererbbar ist.',
+    'Piss off, I tell you something!',
+    'War Daniel eigentlich beim Frisör.',
+    'Sowas von 80er...',
+    'Lösen durch Anstarren.'
+    ]
   haml :index
 end
 
